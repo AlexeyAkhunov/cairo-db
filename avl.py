1b1f577dde0742034d1b43afffe5ef26afb7b4fe
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-07-08 19:02:30 +0100

    First commit

---
+++ avl.py
@@ -0,0 +1,12 @@
+# Operations with AVL trees
+
+from dataclasses import dataclass
+import random
+@dataclass
+class AvlItem:
+    key: int
+    val: int
+
+def generate_initial_set()
+
+avlItem = AvlItem(key=4, val=5)
