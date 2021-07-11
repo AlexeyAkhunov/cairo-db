f52faf81b33f520f4a5fd926f347e4f2d5798fcf
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-07-11 08:30:13 +0100

    nest trees

--- avl.py
+++ avl.py
@@ -1,14 +1,17 @@
 # Operations with AVL trees
 
 from dataclasses import dataclass
+from functools import cmp_to_key
 import random
 
 # generates initial set of composite keys and correcsponding values pseudorandomly, given initial setting
 # it writes the initial set into a file with the name initial_set.txt
 # each entry is a space separated list of the following format
-# {field} {key1} {key2} ... {keyn} {value}
+# {contract} {attribute} {field} {key1} {key2} ... {keyn} {value}
 # for fields that are primitive values it is just {field} {value}
 def generate_initial_set():
+    num_contracts = 1
+    num_attributes = 1
     num_fields = 100
     max_number = 256 # upper bound on numbers generated as keys and values
     min_mapping_size = 1 # minimum number of keys in a mapping
@@ -17,44 +20,47 @@ def generate_initial_set():
     # 0 means field has a primitive value, 1 - mapping, 2 - mapping of mappings, 3 - mapping of mappings of mapping
     composite_lengths = random.choices([0, 1, 2, 3], weights = [8, 4, 2, 1], k=num_fields)
     with open("initial_set.txt", "w") as f:
-        for field in range(num_fields):
-            comp_length  = composite_lengths[field]
-            if comp_length == 0:
-                # Just a primitive value, generate
-                v = random.randrange(0, max_number)
-                f.write(f'{field} {v}\n')
-            else:
-                keys = [random.randrange(0, max_number) for i in range(comp_length)] # components of the current composite key
-                countdown = [random.randrange(min_mapping_size, max_mapping_size) for i in range(comp_length)] # Counting down number of keys that we still need to generate on certain level, 0 means it is done
-                while countdown[0] > 0:
-                    # Output field, keys and value
-                    f.write(f'{field} ')
-                    for k in keys:
-                        f.write(f'{k} ')
-                    v = random.randrange(0, max_number)
-                    f.write(f'{v}\n')
-                    # Move on to the next sequence of keys
-                    i = len(countdown) - 1
-                    while i >= 0:
-                        countdown[i]-=1
-                        keys[i] = random.randrange(0, max_number)
-                        if countdown[i] > 0:
-                            break
-                        if i == 0:
-                            break
-                        countdown[i] = random.randrange(min_mapping_size, max_mapping_size)
-                        i-=1
-                f.write(f'{field} {composite_lengths[field]}\n')
+        for contract in range(num_contracts):
+            for attribute in range(num_attributes):
+                for field in range(num_fields):
+                    comp_length  = composite_lengths[field]
+                    if comp_length == 0:
+                        # Just a primitive value, generate
+                        v = random.randrange(0, max_number)
+                        f.write(f'{contract} {attribute} {field} {v}\n')
+                    else:
+                        #keys = [random.randrange(0, max_number) for i in range(comp_length)] # components of the current composite key
+                        countdown = [random.randrange(min_mapping_size, max_mapping_size) for i in range(comp_length)] # Counting down number of keys that we still need to generate on certain level, 0 means it is done
+                        # For each part of the composite key, pre-generate keys by sampling without replacement (to avoid duplicates)
+                        key_samples = [random.sample(range(max_number), k=k) for k in countdown]
+                        while countdown[0] > 0:
+                            # Output field, keys and value
+                            f.write(f'{contract} {attribute} {field} ')
+                            for i, key_sample in enumerate(key_samples):
+                                f.write(f'{key_sample[countdown[i]-1]} ')
+                            v = random.randrange(0, max_number)
+                            f.write(f'{v}\n')
+                            # Move on to the next sequence of keys
+                            i = len(countdown) - 1
+                            while i >= 0:
+                                countdown[i]-=1
+                                if countdown[i] > 0:
+                                    break
+                                if i == 0:
+                                    break
+                                k = random.randrange(min_mapping_size, max_mapping_size)
+                                countdown[i] = k
+                                key_samples[i] = random.sample(range(max_number), k=k)
+                                i-=1
 
 @dataclass
-class AvlItem:
+class AvlNode:
     key: list # potentially composite key
-    val: int
-    # hint for encoding the structure of the tree. Assuming that the tree is built from the sorted
-    # sequence of keys using stack based algorith, branches specifies how many times, after this
-    # node is pushed to the stack, two nodes need to be taken from the stack and connected
-    # via branch that is put back onto the stack
-    branches: int
+    depth: int # length of path from root of the tree to 
+    path: int # path - sequence of 0 (left) or 1 (right) bits specifying how to get from root
+    tree: bool # set to True if this node is root of the tree for the nested data structure
+    val: int # primitive value for the node (mutually exclusive with subtree)
+    subtree: list # complex value for the node (mutually exclusive with val)
 
 # reads initial set from the file initial_set.txt and build initial avl tree ensuring it is balanced
 def build_initial_tree():
@@ -79,7 +85,36 @@ def build_initial_tree():
                 return 1
             i += 1
         return 0
-    sorted_items = sorted(items, cmp=compare)
+    items.sort(key=cmp_to_key(compare))
+    tree_stack = [[]] # Stack of trees being built, put empty tree on the top
+    prefix_stack = [[]] # Stack of the composite key prefixes corresponding to the tree on the tree_stack, put empty prefix on the top
+    for item in items:
+        num_keys = len(item) - 1
+        # check if the prefix of the item matches to the prefix on top of the stack
+        while num_keys < len(prefix_stack[-1]) or item[:len(prefix_stack[-1])] != prefix_stack[-1]:
+            # tree on the top of the stack is complete, need to pop it
+            prefix_stack.pop(-1)
+            tree_stack.pop(-1)
+        # now check if we need to create nested tree
+        if num_keys > len(prefix_stack[-1]) + 1:
+            nested_tree = []
+            nested_key = item[:len(prefix_stack[-1]) + 1]
+            tree_stack[-1].append(AvlNode(key=nested_key, depth=0, path=0, tree=True, subtree=nested_tree, val=0)) # depth and path is determined during balancing
+            tree_stack.append(nested_tree)
+            prefix_stack.append(nested_key)
+        # now simply add a new node to the tree which is on top of the tree stack
+        tree_stack[-1].append(AvlNode(key=item[:num_keys], depth=0, path=0, tree=False, subtree=None, val=item[-1])) # depth and path is determined during balancing
+    main_tree = tree_stack[0]
+    print_tree("", main_tree)
+    # Now balance every sub tree to establish correct depth and path values
+
+def print_tree(indent, nodes):
+    for node in nodes:
+        if node.tree:
+            print(f'{indent}{node.key}')
+            print_tree(indent+"  ", node.subtree)
+        else:
+            print(f'{indent}{node.key} {node.val}')
 
 #generate_initial_set()
 build_initial_tree()
